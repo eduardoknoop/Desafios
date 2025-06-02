bd6684d924999a679a4d8de135955483577bdfe4
Eduardo Knoop <eduardoo.knoop@gmail.com>
2025-06-02 15:06:29 -0300

    Lista js

---
+++ app.js
@@ -0,0 +1,7 @@
+let listaGenerica = [];
+let listaProgramação = ['JavaScript','C','C++','Kotlin','Python'];
+listaProgramação.push('Java', 'Ruby', 'GoLang');
+console.log(listaProgramação);
+
+let nomes = ['junio bixa', 'augusto gonzaga', 'roberto'];
+console.log(nomes[0]);
